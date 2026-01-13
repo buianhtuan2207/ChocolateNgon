eb695560723f1f4b5371cc94f2061006f783e658
buianhtuan2207 <22130309@st.hcmuaf.edu.vn>
2026-01-14 01:20:07 +0700

    feat: create component OrderStepper and OrderHistory

--- src/pages/OrderTracking/OrderTracking.tsx
+++ src/pages/OrderTracking/OrderTracking.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./orderTracking.scss"
 import StatusCard from "../../components/StatusCard/StatusCard";
+import OrderStepper from "../../components/OrderStepper/OrderStepper";
+import OrderHistory from "../../components/OrderHistory/OrderHistory";
 
 const OrderTracking = () => {
     return (
@@ -17,6 +19,13 @@ const OrderTracking = () => {
                         <StatusCard label="Trạng Thái" icon="truck-fast" value="Đang vận chuyển" statusType="action"/>
                         <StatusCard label="Dự kiến giao" value="3 ngày giao tới" icon="Calender" statusType={undefined}/>
                         <StatusCard label="Đơn vị vận chuyển" value="giaohangnhanh" icon="box" statusType={undefined}/>
+                    </div>
+                    <div className="tracking-col col-2">
+                        <h3>Lộ trình vận chuyển</h3>
+                        <OrderStepper currentStep={3}/>
+                        <div className="history-list">
+                            <OrderHistory/>
+                        </div>
                     </div>
 
                 </div>
