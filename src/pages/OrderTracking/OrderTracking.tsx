81b1f09926967fde5faf54dde0a2129d0d8da279
buianhtuan2207 <22130309@st.hcmuaf.edu.vn>
2026-01-14 02:46:44 +0700

    feat: complate ui OrderTracking page

--- src/pages/OrderTracking/OrderTracking.tsx
+++ src/pages/OrderTracking/OrderTracking.tsx
@@ -4,24 +4,25 @@ import StatusCard from "../../components/StatusCard/StatusCard";
 import OrderStepper from "../../components/OrderStepper/OrderStepper";
 import OrderHistory from "../../components/OrderHistory/OrderHistory";
 import OrderProductList from "../../components/OrderProductList/OrderProductList";
+import DeliveryInfo from "../../components/DeliveryInfo/DeliveryInfo";
 
 const OrderTracking = () => {
     return (
         <div className="orderTracking-page">
+            <div className="order-tracking-header">
+                <h1>Theo dõi đơn hàng</h1>
+                <p className="order-meta">
+                    Mã vận đơn: <strong>#CHOC-8829-2023</strong> | Đặt ngày 20/10/2023
+                </p>
+            </div>
             <div className="order-tracking container">
-                <div className="order-tracking-header">
-                    <h1>Theo dõi đơn hàng</h1>
-                    <p className="order-meta">
-                        Mã vận đơn: <strong>#CHOC-8829-2023</strong> | Đặt ngày 20/10/2023
-                    </p>
-                </div>
                 <div className="left">
-                    <div className="status-col col-1">
+                    <div className="status-col">
                         <StatusCard label="Trạng Thái" icon="truck-fast" value="Đang vận chuyển" statusType="action"/>
                         <StatusCard label="Dự kiến giao" value="3 ngày giao tới" icon="Calender" statusType={undefined}/>
                         <StatusCard label="Đơn vị vận chuyển" value="giaohangnhanh" icon="box" statusType={undefined}/>
                     </div>
-                    <div className="tracking-col col-2">
+                    <div className="tracking-col">
                         <h3>Lộ trình vận chuyển</h3>
                         <OrderStepper currentStep={3}/>
                         <div className="history-list">
@@ -33,7 +34,7 @@ const OrderTracking = () => {
                     </div>
                 </div>
                 <div className="right">
-
+                    <DeliveryInfo/>
                 </div>
             </div>
         </div>
